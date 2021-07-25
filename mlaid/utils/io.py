47db4767ff884039f8fdaeaa3bb5d5684e62e9cc
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-07-25 11:23:44 +0530

    Adds yaml loader and saver

--- mlaid/utils/io.py
+++ mlaid/utils/io.py
@@ -4,6 +4,50 @@ Utilities for input-output loading/saving.
 
 from typing import Any
 import pickle
+import yaml
+
+
+class PrettySafeLoader(yaml.SafeLoader):
+    """Custom loader for reading YAML files"""
+    def construct_python_tuple(self, node):
+        return tuple(self.construct_sequence(node))
+
+
+PrettySafeLoader.add_constructor(
+    u'tag:yaml.org,2002:python/tuple',
+    PrettySafeLoader.construct_python_tuple
+)
+
+
+def load_yml(path: str, loader_type: str = 'default'):
+    """Read params from a yml file.
+
+    Args:
+        path (str): path to the .yml file
+        loader_type (str, optional): type of loader used to load yml files. Defaults to 'default'.
+
+    Returns:
+        Any: object (typically dict) loaded from .yml file
+    """
+    assert loader_type in ['default', 'safe']
+
+    loader = yaml.Loader if (loader_type == "default") else PrettySafeLoader
+
+    with open(path, 'r') as f:
+        data = yaml.load(f, Loader=loader)
+
+    return data
+
+
+def save_yml(data: dict, path: str):
+    """Save params in the given yml file path.
+
+    Args:
+        data (dict): data object to save
+        path (str): path to .yml file to be saved
+    """
+    with open(path, 'w') as f:
+        yaml.dump(data, f, default_flow_style=False)
 
 
 def load_pkl(path: str, encoding: str = "ascii") -> Any:
--- tests/utils/test_io.py
+++ tests/utils/test_io.py
@@ -5,11 +5,11 @@ import unittest
 from os.path import join, isdir
 
 from mlaid.utils.path import repo_path
-from mlaid.utils.io import load_pkl, save_pkl
+from mlaid.utils.io import load_pkl, save_pkl, load_yml, save_yml
 
 
 class TestPickle(unittest.TestCase):
-    """Class to run tests on functions in utils/io.py"""
+    """Class to run tests on `pickle` functions in utils/io.py"""
 
     @classmethod
     def setUpClass(cls):
@@ -27,10 +27,31 @@ class TestPickle(unittest.TestCase):
         self.assertEqual(data, self.data)
 
 
+class TestYAML(unittest.TestCase):
+    """Class to run tests on `yaml` functions in utils/io.py"""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.assets_dir = join(repo_path, "assets")
+        assert isdir(cls.assets_dir)
+
+        cls.data = {"a": 1, "b": 2, "c": 3}
+        cls.yml_path = join(cls.assets_dir, "data.yml")
+    
+    def test_save_yml(self):
+        save_yml(self.data, self.yml_path)
+    
+    def test_load_yml(self):
+        data = load_yml(self.yml_path)
+        self.assertEqual(data, self.data)
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestPickle('test_save_pkl'))
     suite.addTest(TestPickle('test_load_pkl'))
+    suite.addTest(TestYAML('test_save_yml'))
+    suite.addTest(TestYAML('test_load_yml'))
     return suite
 
 
